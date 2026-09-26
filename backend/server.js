68833d0c6a9010b8b502e5232530a432ec9a10e6
nightdotpi <araaajif@gmail.com>
2026-09-26 17:38:26 +0330

    Add new allowed origin to CORS configuration

--- backend/server.js
+++ backend/server.js
@@ -202,6 +202,7 @@ app.use((req, res, next) => {
 const defaultAllowedOrigins = [
   'https://nightdotpi.github.io',
   'https://night9423.pinet.com',
+  'https://pinightpi.github.io',
   'https://nightez2278.pinet.com',
   'https://night.bonto.run',
   'https://sandbox.minepi.com',
